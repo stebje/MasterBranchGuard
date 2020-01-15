09a4b2552fbe803657b7ea46836082ce3ce74b49
Steffen B <steffenbj@hotmail.com>
2020-01-15 20:32:58 +0100

    Allow app.yml to issue comments, updated index.js with sample code, updated private flag in package.json
    Signed-off-by: Steffen B <steffenbj@hotmail.com>

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+/*
+* This app will
+* 1.Automatically protect the master branch of a newly created repo
+* 2.Automatically create an issue which includes the details of the protections added, and a @mention
+* of the current GitHub user
+* */
+
 module.exports = app => {
-  // Your code here
-  app.log('The app is running!')
+  app.log('The app is up and running!')
 
+  // Trigger for when new repos are created
   app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   { owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World! }
+    const params = context.issue({ body: 'This is an auto-generated comment for a new issue!' })
+
+    // Post a comment on the issue
+    return context.github.issues.createComment(params)
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
