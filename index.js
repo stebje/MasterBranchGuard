f452327978c831f4ae8c25d229b9f19b14019672
Steffen B <steffenbj@hotmail.com>
2020-01-15 01:05:10 +0100

    Cosmetic changes and spell check, changed app name in package.json to avoid compile warning on upper case letters
    Signed-off-by: Steffen B <steffenbj@hotmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
 module.exports = app => {
   // Your code here
-  app.log('Yay, the app was loaded!')
+  app.log('The app is running!')
 
   app.on('issues.opened', async context => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
